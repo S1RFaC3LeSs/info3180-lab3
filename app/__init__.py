fc6c8926938a8f431a0b6aa031ef750badcde0ed
Giovanni Lawrence <isekcftw@gmail.com>
2024-02-27 00:39:54 -0400

    These files were modified as intended

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,13 @@
 from flask import Flask
-
+# THESE WERE ADDED
+from flask_mail import Mail
+from .config import Config
+#
 app = Flask(__name__)
+
+# THESE WERE ADDED
+app.config.from_object(Config) 
+mail = Mail(app)
+#
+
 from app import views
